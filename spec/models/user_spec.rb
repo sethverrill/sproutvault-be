3791794620c69f53c0f3f5f1af4b495c57035230
sethverrill <verrill.seth@gmail.com>
2025-03-16 17:19:29 -0600

    fixes to solve failing tests on GitHub Actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email).case_insensitive}
+    it { should validate_uniqueness_of(:email).case_insensitive }
     it { should allow_value("validtest@email.com").for(:email) }
     it { should_not allow_value("invalid-email-address").for(:email) }
     it { should validate_length_of(:password).is_at_least(8) }
