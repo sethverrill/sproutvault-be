5c066ec5f858e821884d65430d776dd22ba334f5
sethverrill <verrill.seth@gmail.com>
2025-03-16 18:24:19 -0600

    adjusting eager loader to fix CI vs local issue

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   # this is usually not necessary, and can slow down your test suite. However, it's
   # recommended that you enable it in continuous integration systems to ensure eager
   # loading is working properly before deploying your code.
+  config.eager_load = true
   config.eager_load = ENV["CI"].present?
 
   # Configure public file server for tests with cache-control for performance.
