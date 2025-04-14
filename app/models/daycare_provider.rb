79f5fdea5ffd574ef3f9e648f8ea6c8966453f7b
sethverrill <verrill.seth@gmail.com>
2025-04-14 11:32:39 -0600

    added enum to to daycare provider to handle provider type

--- app/models/daycare_provider.rb
+++ app/models/daycare_provider.rb
@@ -1,4 +1,11 @@
 class DaycareProvider < ApplicationRecord
+  enum provider_type: {
+    daycare: 0,
+    nanny: 1,
+    family: 2,
+    parent: 3,
+    other: 4
+  }
   has_many :daycare_hours, dependent: :destroy
   has_many :children, through: :daycare_hours
 
