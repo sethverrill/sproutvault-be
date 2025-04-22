dcf2a1c14ab72f9961a949566dcad79c817260ad
sethverrill <verrill.seth@gmail.com>
2025-04-22 15:23:28 -0600

    Update Application Controller with authorize request for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
-class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+class ApplicationController < ActionController::API
   allow_browser versions: :modern
+  before_action :authorize_request
+
+  private
+
+  def authorize_request
+    header = request.headers["Authorization"]
+    token = header&.split(" ")&.last
+    decoded = JsonWebToken.decode(token)
+    @current_user = User.find(decoded[:user_id])
+  rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+    render json: { error: "Unauthorized" }, status: :unauthorized
+  end
 end
